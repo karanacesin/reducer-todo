6f53d790719e020895b900bc964cc3d8dcc48d26
Dawna Backmeier <karanacesin@yahoo.com>
2020-06-09 19:22:43 -0400

    modified

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,13 @@
+export const initialState = {
+    item: '',
+    completed: false,
+    id: new Date(),
+  };
+  
+  export const reducer = (state, action) => {
+    switch (action.type) {
+      
+      default:
+        return state;
+    }
+  };
